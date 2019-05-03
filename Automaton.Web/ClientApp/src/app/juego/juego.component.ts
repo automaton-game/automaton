4159a65d394ea68250dfb16d0bbf4206054cf3c3
Hernan Lavrencic <hlavrencic@baufest.com>
2019-05-03 19:11:40 -0300

    Mejoro la logica de robot defensivo

--- Automaton.Web/ClientApp/src/app/juego/juego.component.ts
+++ Automaton.Web/ClientApp/src/app/juego/juego.component.ts
@@ -31,80 +31,129 @@ export class JuegoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const logica = `
-using Automaton.Contratos.Entorno;
-using Automaton.Contratos.Helpers;
-using Automaton.Contratos.Robots;
-using System;
-using System.Collections.Generic;
-
-namespace Automaton.Robots
+    const logica = `namespace Automaton.Logica.Robots
 {
-    public class RobotUsuario : IRobot
+    using Automaton.Contratos.Entorno;
+    using Automaton.Contratos.Helpers;
+    using Automaton.Contratos.Robots;
+    
+    /// <summary>
+    /// El nombre de la clase es el nombre del usuario. 
+    /// Por lo tanto siempre debes utilizar el mismo nombre en la creación de tu clase
+    /// </summary>
+    /// <example> MiRobotJose </example>
+    public class RobotDefensivo : IRobot
     {
+        ///<summary>Devuelve el estado actual del tablero. Correspondiente al turno actual</summary>
         public Tablero Tablero { get; set; }
 
+        ///<summary>El metodo que resuelve el proximo movimiento que el robot debe ejecutar</summary>
+		///<returns>Devuelve una accion según el estado actual del tablero.</returns>
         public AccionRobotDto GetAccionRobot()
         {
-            var casillero = this.GetPosition(Tablero);
+            /// -------------------------------------
+            /// Completar aqui con tu logica
+            /// -------------------------------------
+
+            /// -------------------------------------
+            /// Prueba distintos ejemplos de movimiento --->
+            
+
+            return MovimientoEjemplo1();
+            //return MovimientoEjemplo2();
+            //return MovimientoEjemplo3();
+        }
+
+        private AccionRobotDto MovimientoEjemplo1()
+        {
+            // Obtengo el casillero donde estoy parado
+            var casillero = this.GetPosition();
+
+            // Verifico si ya construi una muralla. Sino envio la orden de construir una
             if (casillero.Muralla == null && casillero.Robots.Count == 1)
             {
                 return new AccionConstruirDto() { };
             }
 
-            var direcciones = new List<DireccionEnum>();
-
-            var direccion = GenerarDireccionAleatoria(direcciones);
-            var movimiento = EvaluarMovimiento(casillero, direccion);
-            while (movimiento == null)
+            //Si tengo lugar, intento moverme para arriba
+            if (this.BuscarRelativo(DireccionEnum.Arriba) != null)
             {
-                direcciones.Add(direccion);
-                if (direcciones.Count >= 4)
-                {
-                    return null;
-                }
-
-                direccion = GenerarDireccionAleatoria(direcciones);
-                movimiento = EvaluarMovimiento(casillero, direccion);
+                return new AccionMoverDto { Direccion = DireccionEnum.Arriba };
+            }
+            // Sino, intento moverme para abajo
+            else if (this.BuscarRelativo(DireccionEnum.Abajo) != null)
+            {
+                return new AccionMoverDto { Direccion = DireccionEnum.Abajo };
             }
 
-            return movimiento;
-        }
-
-        private AccionMoverDto EvaluarMovimiento(Casillero casillero, DireccionEnum direccion)
-        {
-            var relativo = casillero.BuscarRelativo(direccion);
-            if (relativo != null)
-            {
-                if (relativo.Muralla == null || relativo.Muralla == this)
-                {
-                    return new AccionMoverDto() { Direccion = direccion };
-                }
-            }
-
+            // Si no se que hacer, devuelvo nulo. Y el juego termina.
             return null;
         }
 
-        private DireccionEnum GenerarDireccionAleatoria()
+        private AccionRobotDto MovimientoEjemplo2()
         {
-            var random = new Random().Next(0,4);
-            return (DireccionEnum)random;
-        }
+            // Obtengo el casillero donde estoy parado
+            var casillero = this.GetPosition();
 
-        private DireccionEnum GenerarDireccionAleatoria(IList<DireccionEnum> evitar)
-        {
-            var obtenido = GenerarDireccionAleatoria();
-            while (evitar.Contains(obtenido))
+            // Verifico si ya construi una muralla. Sino envio la orden de construir una
+            if (casillero.Muralla == null && casillero.Robots.Count == 1)
             {
-                obtenido = GenerarDireccionAleatoria();
+                return new AccionConstruirDto() { };
             }
 
-            return obtenido;
+            //Si tengo lugar, intento moverme para arriba
+            if (this.BuscarRelativo(DireccionEnum.Arriba) != null && this.BuscarRelativo(DireccionEnum.Arriba).Muralla == null)
+            {
+                return new AccionMoverDto { Direccion = DireccionEnum.Arriba };
+            }
+            // Sino, intento moverme para abajo
+            else if (this.BuscarRelativo(DireccionEnum.Abajo) != null && this.BuscarRelativo(DireccionEnum.Abajo).Muralla == null)
+            {
+                return new AccionMoverDto { Direccion = DireccionEnum.Abajo };
+            }
+
+            // Si no se que hacer, devuelvo nulo. Y el juego termina.
+            return null;
+        }
+
+        private AccionRobotDto MovimientoEjemplo3()
+        {
+            // Obtengo el casillero donde estoy parado
+            var casillero = this.GetPosition();
+
+            // Verifico si ya construi una muralla. Sino envio la orden de construir una
+            if (casillero.Muralla == null && casillero.Robots.Count == 1)
+            {
+                return new AccionConstruirDto() { };
+            }
+
+            //Si tengo lugar, intento moverme para arriba
+            if (this.BuscarRelativo(DireccionEnum.Arriba) != null && this.BuscarRelativo(DireccionEnum.Arriba).Muralla == null)
+            {
+                return new AccionMoverDto { Direccion = DireccionEnum.Arriba };
+            }
+            // Sino, intento moverme para abajo
+            else if (this.BuscarRelativo(DireccionEnum.Abajo) != null && this.BuscarRelativo(DireccionEnum.Abajo).Muralla == null)
+            {
+                return new AccionMoverDto { Direccion = DireccionEnum.Abajo };
+            }
+            // Sino, intento moverme para abajo
+            else if (this.BuscarRelativo(DireccionEnum.Izquierda) != null && this.BuscarRelativo(DireccionEnum.Izquierda).Muralla == null)
+            {
+                return new AccionMoverDto { Direccion = DireccionEnum.Izquierda };
+            }
+            // Sino, intento moverme para abajo
+            else if (this.BuscarRelativo(DireccionEnum.Derecha) != null && this.BuscarRelativo(DireccionEnum.Derecha).Muralla == null)
+            {
+                return new AccionMoverDto { Direccion = DireccionEnum.Derecha };
+            }
+
+
+            // Si no se que hacer, devuelvo nulo. Y el juego termina.
+            return null;
         }
     }
-}
-
-    `;
+}`;
 
     this.logica1 = logica;
   }
